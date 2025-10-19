13b7c6092ab0710b7a863e00844c120f9d48d92f
ParthaPradeepNath <parthanath96134@gmail.com>
2025-10-19 15:01:59 +0530

    polar payment integrated

--- src/components/app-header.tsx
+++ src/components/app-header.tsx
@@ -3,9 +3,7 @@ import { SidebarTrigger } from "./ui/sidebar";
 export const AppHeader = () => {
   return (
     <header className="flex items-center gap-2 border-b h-14 shrink-0 px-4 bg-background">
-        <SidebarTrigger />
-
+      <SidebarTrigger />
     </header>
   );
-};  
-
+};
